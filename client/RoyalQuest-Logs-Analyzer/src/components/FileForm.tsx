4192b7e7d75958f88902f675ef96f910f356990d
KoatKoetl <stas20027@outlook.com>
2024-04-04 19:51:59 +0300

    Add custom submit to Form

--- client/RoyalQuest-Logs-Analyzer/src/components/FileForm.tsx
+++ client/RoyalQuest-Logs-Analyzer/src/components/FileForm.tsx
@@ -1,16 +1,34 @@
 const FileForm = () => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    try {
+      const response = await fetch('http://localhost:3000/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      if (response.ok) {
+        alert('File successfully sent.');
+      } else {
+        alert(`Error sending file: ${response.statusText}`);
+      }
+    } catch (error) {
+      alert(`Error sending file:, ${error}`);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-full font-Bona-Nova">
-      <form action="" method="" className="flex flex-col justify-center items-center sm:p-6 gap-2">
+      <form onSubmit={handleSubmit} encType="multipart/form-data" className="flex flex-col justify-center items-center sm:p-6 gap-2">
         <h1 className="text-center font-bold mb-8 bg-clip-text">
           <span className="md:text-6xl royalQuestGradient p-2">Royal Quest </span>
           <span className="md:text-6xl royalQuestGradient p-2">Log Scanner</span>
         </h1>
         <div className="flex gap-2 border-transparent border-b-2 border-t-2 hover:border-[#ce9b50] p-1 transition-colors">
-          <label htmlFor="logFile" className="sm:text-2xl font-bold">
+          <label htmlFor="file" className="sm:text-2xl font-bold">
             Select a log file to scan:
           </label>
-          <input type="file" id="logFile" name="logFile" accept="text/htm, text/html" required />
+          <input type="file" id="logFile" name="file" accept="text/htm, text/html" required />
         </div>
         <button
           type="submit"
