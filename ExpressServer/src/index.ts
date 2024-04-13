22a000c5377a60438018ff395cfe725bd740c882
KoatKoetl <stas20027@outlook.com>
2024-04-13 21:41:54 +0300

    Fix (static pages, post for DB request, new middlewares)

--- ExpressServer/src/index.ts
+++ ExpressServer/src/index.ts
@@ -1,27 +1,45 @@
+import compression from 'compression';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import multer from 'multer';
 import path from 'path';
 import { badRequestHandler, getDownloadsFile, getDownloadsFolder } from './GET';
 import { fileFilter } from './extensionValidation';
-import { handleFileUpload } from './handleFileUpload';
+import { handleFileUpload_StoreItemsOnServer } from './handleFileUpload';
 import { storage } from './multerStorage';
+import { handleFileUpload_StoreItemsInDB } from './utils/StoreItemsInDB';
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+});
 
-app.use(express.static(path.join('../client/RoyalQuest-Logs-Analyzer/dist')));
+app.use('/', express.static(path.join('../client/RoyalQuest-Logs-Analyzer/dist')));
 app.use(cors());
+app.use(compression());
+app.use(limiter);
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-app.post('/uploads', upload.single('file'), handleFileUpload);
+app.post('/uploads', upload.single('file'), handleFileUpload_StoreItemsOnServer);
+app.post('/storeInDB', upload.single('file'), handleFileUpload_StoreItemsInDB);
 
 app.get('/downloads', getDownloadsFolder);
 app.get('/downloads/:filename', getDownloadsFile);
 app.get('*', badRequestHandler);
+app.get('/getFilesPage', (req, res) => {
+  const filePath = path.resolve('../client/RoyalQuest-Logs-Analyzer/dist/index.html');
+  res.sendFile(filePath);
+});
+app.get('/getFilesPage/ItemsTable', (req, res) => {
+  const filePath = path.resolve('../client/RoyalQuest-Logs-Analyzer/dist/index.html');
+  res.sendFile(filePath);
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
