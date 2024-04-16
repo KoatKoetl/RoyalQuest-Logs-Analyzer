1b9923e5f592a4a949a118ba5a3a0102c550cb71
KoatKoetl <stas20027@outlook.com>
2024-04-16 20:38:31 +0300

    Fix (change used functions and imports, connected to MongoDB)

--- ExpressServer/src/index.ts
+++ ExpressServer/src/index.ts
@@ -1,15 +1,18 @@
 import compression from 'compression';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
+import mongoose from 'mongoose';
 import multer from 'multer';
 import path from 'path';
-import { badRequestHandler, getDownloadsFile, getDownloadsFolder } from './GET';
-import { fileFilter } from './extensionValidation';
-import { handleFileUpload_StoreItemsOnServer } from './handleFileUpload';
-import { storage } from './multerStorage';
-import { handleFileUpload_StoreItemsInDB } from './utils/StoreItemsInDB';
+import { badRequestHandler, getDownloadsFile, getDownloadsFolder } from './GET.js';
+import storeMongoDB from './MongoDB/addItemsInDB.js';
+import { router as ItemRouter } from './MongoDB/itemRouters.js';
+import { fileFilter } from './extensionValidation.js';
+import { handleFileUpload_StoreItemsOnServer } from './handleFileUpload.js';
+import { storage } from './multerStorage.js';
 
 const app = express();
 dotenv.config();
@@ -19,15 +22,32 @@ const limiter = rateLimit({
   max: 100,
 });
 
+const mongo_URL = 'mongodb://localhost:27017/RoyalQuest';
+
+async function connectToMongoDB(connectionString: string) {
+  await mongoose.connect(connectionString);
+  console.log('Connected to MongoDB');
+}
+
+try {
+  await connectToMongoDB(mongo_URL);
+} catch (e) {
+  console.log(e);
+}
+
 app.use('/', express.static(path.join('../client/RoyalQuest-Logs-Analyzer/dist')));
 app.use(cors());
 app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(limiter);
+app.use('/api', ItemRouter);
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 app.post('/uploads', upload.single('file'), handleFileUpload_StoreItemsOnServer);
-app.post('/storeInDB', upload.single('file'), handleFileUpload_StoreItemsInDB);
+app.post('/addItemsInDB', upload.single('file'), storeMongoDB);
 
 app.get('/downloads', getDownloadsFolder);
 app.get('/downloads/:filename', getDownloadsFile);
