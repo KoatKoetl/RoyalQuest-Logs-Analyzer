62a4cd23a5746f40bc409e9b81f79ef8de5a767e
KoatKoetl <stas20027@outlook.com>
2024-04-18 22:13:11 +0300

    Change (Create function to make mongoose models)

---
+++ ExpressServer/src/models/itemSchemas.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose';
+
+const itemSchema = new mongoose.Schema({
+  _id: String,
+  name: { type: String, required: true, unique: true },
+  type: String,
+  rarity: String,
+  rarityColor: String,
+  marketPrice: Number,
+  NPCPrice: Number,
+  amount: { type: Number, default: undefined },
+});
+
+const createMongooseModel = (collectionName: string, itemSchema: Schema) => {
+  const newCollection = mongoose.model(collectionName, itemSchema);
+  return newCollection;
+};
+
+export { createMongooseModel, itemSchema };
