239281f48f512c37a6d8b158721cac3525bae08f
KoatKoetl <stas20027@outlook.com>
2024-04-18 22:08:20 +0300

    Change (Store all data on mongoDB)

---
+++ ExpressServer/src/controllers/addToDatabase.ts
@@ -0,0 +1,42 @@
+import { Response } from 'express';
+import { ItemsDataExtended } from '../models/ItemInterface.js';
+
+const storeAllItemsInDB = async (itemsData: {}[], res: Response, itemModel: any) => {
+  try {
+    const bulkOps = itemsData.map((item: ItemsDataExtended) => ({
+      insertOne: { document: item },
+    }));
+    const result = await itemModel.bulkWrite(bulkOps, { ordered: false });
+    res.status(200).send({
+      success: 'Succesfully added new game items in database',
+      insertedCount: 'Total number of new items added: ' + result.insertedCount,
+    });
+  } catch (error: any) {
+    if (error.code === 11000) {
+      console.log('Duplicate key error in allgameitems. Code:', error.code);
+      res.status(200).send({ success: 'New items succesfully added. Duplicates are skipped' });
+    } else {
+      console.log('MongoDB write error.', error);
+      res.status(500).send({ error: 'Internal Server Error' });
+    }
+  }
+};
+
+const storeCurrentLog = async (itemsData: {}[], res: Response, itemModel: any) => {
+  try {
+    const bulkOps = itemsData.map((item: ItemsDataExtended) => ({
+      updateOne: {
+        filter: { name: item.id },
+        update: { $set: { ...item } },
+        upsert: true,
+      },
+    }));
+    const result = await itemModel.bulkWrite(bulkOps);
+    res.status(200).send({ success: 'Succesfully added current log items', insertedCount: 'Total items added: ' + result.insertedCount });
+  } catch (error: any) {
+    console.log('MongoDB write error.', error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
+
+export { storeAllItemsInDB, storeCurrentLog };
