3e7e6f605c46fe64aab43777eec1d5d22a50dcf3
KoatKoetl <stas20027@outlook.com>
2024-04-19 19:59:15 +0300

    Fix (Add Try catch block to find errors)

--- ExpressServer/src/controllers/getDatabaseCollections.ts
+++ ExpressServer/src/controllers/getDatabaseCollections.ts
@@ -4,17 +4,22 @@ import connectToMongoDB from './connectToDB.js';
 
 const getDatabaseCollections = (req: Request, res: Response) => {
   connectToMongoDB().then(async () => {
-    const db = mongoose.connection.db;
-    const collections = await db.listCollections().toArray();
     const collectionNames: string[] = [];
 
-    collections.forEach((collection) => {
-      const collectionName = collection.name.match(/^(currentlogfile)/i);
-      if (collectionName) {
-        collectionNames.push(collection.name);
-      }
-    });
-    res.status(200).json(collectionNames);
+    try {
+      const db = mongoose.connection.db;
+      const collections = await db.listCollections().toArray();
+      collections.forEach((collection) => {
+        const collectionName = collection.name.match(/^(currentlogfile)/i);
+        if (collectionName) {
+          collectionNames.push(collection.name);
+        }
+      });
+
+      res.status(200).json(collectionNames);
+    } catch (err) {
+      res.status(500).send({ error: 'No database connection. ' + err });
+    }
   });
 };
 
