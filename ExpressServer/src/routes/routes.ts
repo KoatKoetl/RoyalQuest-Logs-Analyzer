f063ba3298b3c26268b1d0547239439700040f30
KoatKoetl <stas20027@outlook.com>
2024-04-19 21:33:10 +0300

    Fix (New route endpoint for get collection items)

--- ExpressServer/src/routes/routes.ts
+++ ExpressServer/src/routes/routes.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
 import multer from 'multer';
 import { findItemByIDinDB, findItemInDB, postItemInDB } from '../controllers/CRUD-Operations.js';
-import { getDownloadsFile, getDownloadsFolder } from '../controllers/GET.js';
 import { fileFilter } from '../controllers/extensionValidation.js';
+import getCollectionItems from '../controllers/getCollectionItems.js';
 import getDatabaseCollections from '../controllers/getDatabaseCollections.js';
 import storeMongoDB_currentLogs from '../controllers/uploadCurrentLog.js';
 import storeMongoDB_allitems from '../controllers/uploadUniqueItemsInDB.js';
@@ -15,10 +15,8 @@ router.post('/storeToAllItems', upload.single('file'), (req: Request, res: Respo
 router.post('/storeCurrentLogs', upload.single('file'), (req: Request, res: Response) => storeMongoDB_currentLogs(req, res));
 router.post('/addItem', (req: Request, res: Response) => postItemInDB(req, res));
 
-router.get('/downloads', (req: Request, res: Response) => getDownloadsFolder(req, res));
 router.get('/databasecollections', (req: Request, res: Response) => getDatabaseCollections(req, res));
-// get collection
-router.get('/downloads/:filename', (req: Request, res: Response) => getDownloadsFile(req, res));
+router.get('/collectionitems/:filename', (req: Request, res: Response) => getCollectionItems(req, res));
 router.get('/getItems', (req: Request, res: Response) => findItemInDB(req, res));
 router.get('/:id', (req: Request, res: Response) => findItemByIDinDB(req, res));
 
