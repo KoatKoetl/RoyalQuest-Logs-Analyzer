eacac1a0e6e11db485df1fdf26ab035e2e3737c9
KoatKoetl <stas20027@outlook.com>
2024-04-19 21:32:14 +0300

    Add (Function to get the item from specific DB collection)

---
+++ ExpressServer/src/controllers/getCollectionItems.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { createMongooseModel, itemSchema } from '../models/itemSchemas.js';
+
+const getCollectionItems = async (req: Request, res: Response) => {
+  const filename = req.params.filename;
+  const itemModel = createMongooseModel(filename, itemSchema);
+
+  try {
+    const items = await itemModel.find().exec();
+    res.status(200).json(items);
+  } catch (err) {
+    res.status(500).json({ err: err });
+  }
+};
+
+export default getCollectionItems;
