54e4a4e520c4660d50a7cf0f0db8994d6d3e073c
KoatKoetl <stas20027@outlook.com>
2024-04-29 16:35:52 +0300

    Fix (image path)

--- client/RoyalQuest-Logs-Analyzer/src/components/gameLogo.tsx
+++ client/RoyalQuest-Logs-Analyzer/src/components/gameLogo.tsx
@@ -4,7 +4,7 @@ const GameLOGO = () => {
       <h1 className="text-center font-bold bg-clip-text">
         {/* <span className="md:text-6xl royalQuestGradient p-2">Royal Quest </span>
         <span className="md:text-6xl royalQuestGradient p-2">Log Scanner</span> */}
-        <img src="/royalQuestLogo.png" alt="Royal Quest game logo" />
+        <img src="/images/royalQuestLogo.png" alt="Royal Quest game logo" />
       </h1>
     </>
   );
