42be7e3116afbd8aaca49c0d20a4df1b9bdf8348
KoatKoetl <stas20027@outlook.com>
2024-05-01 23:27:32 +0300

    Create (login endpoint)

--- ExpressServer/src/routes/authRoutes.ts
+++ ExpressServer/src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import registerNewUser from '../controllers/CRUD-operations/auth/addNewUser.js';
+import loginUser from '../controllers/CRUD-operations/auth/loginUser.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', (req: Request, res: Response) => registerNewUser(req, res));
+authRouter.post('/login', (req: Request, res: Response) => loginUser(req, res));
 
 export default authRouter;
