2f6b02b9e702f848e8c8cea4f8ae9be0b2504366
KoatKoetl <stas20027@outlook.com>
2024-05-03 20:22:35 +0300

    Fix (path to logout)

--- ExpressServer/src/routes/authRoutes.ts
+++ ExpressServer/src/routes/authRoutes.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from 'express';
 import registerNewUser from '../controllers/CRUD-operations/auth/addNewUser.js';
-import getRefreshToken, { logout } from '../controllers/CRUD-operations/auth/getRefreshToken.js';
+import getRefreshToken from '../controllers/CRUD-operations/auth/getRefreshToken.js';
 import loginUser from '../controllers/CRUD-operations/auth/loginUser.js';
+import userLogout from '../controllers/CRUD-operations/auth/logoutUser.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', (req: Request, res: Response) => registerNewUser(req, res));
 authRouter.post('/login', (req: Request, res: Response) => loginUser(req, res));
 authRouter.post('/token', (req: Request, res: Response) => getRefreshToken(req, res));
-authRouter.delete('/logout', (req: Request, res: Response) => logout(req, res));
+authRouter.delete('/logout', (req: Request, res: Response) => userLogout(req, res));
 
 export default authRouter;
